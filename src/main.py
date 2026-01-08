47575810605ee590db93ceb5f236660745bfb555
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2026-01-08 14:03:19 +0300

    Создание роута для создания эндпоинта на создание Контакта и роута.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from notification-service!")
+from fastapi import FastAPI
+from src.api.api_contacts import router as contacts_router
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(title="Mailing System API", version="1.0.0")
+
+# Подключаем роутеры
+app.include_router(contacts_router)
+
+@app.get("/")
+async def root():
+    return {"message": "Добро пожаловать в Mailing System API"}
+
