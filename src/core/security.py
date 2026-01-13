48d020e558841d1d7ab71fca7a7cee01a1ff78e5
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2026-01-13 12:19:03 +0300

    Добавление файла для работы с паролями и JWT.

---
+++ src/core/security.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+from typing import Optional, Union, Any
+import jwt
+from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+# Настройка хеширования паролей
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Проверка соответствия пароля и его хеша"""
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password: str) -> str:
+    """Создание хеша из пароля"""
+    return pwd_context.hash(password)
+
+
+def create_access_token(
+        subject: Union[str, Any], expires_delta: Optional[timedelta] = None
+    ) -> str:
+    """Создание JWT токена"""
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expire, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
